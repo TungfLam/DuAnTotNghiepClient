0278e0fab96de810b0b37dd2ff215d45d84830d0
tran van trong <trongtvph21826@fpt.edu.vn>
2023-11-05 14:34:31 +0700

    call_ api_product

--- lib/models/ProductList.dart
+++ lib/models/ProductList.dart
@@ -1,6 +1,161 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'Product.dart';
+// import 'Product.dart';
+
+// class ProductList extends StatefulWidget {
+//   const ProductList({super.key});
+
+//   @override
+//   _ProductListState createState() => _ProductListState();
+// }
+
+// class _ProductListState extends State<ProductList> {
+//   List<Product> products = [
+//     Product(
+//         name: 'Pink Hoodie',
+//         price: '100 USD',
+//         imageUrl: 'lib/images/hoodie2.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '75 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '50 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     Product(
+//         name: 'Pink Hoodie',
+//         price: '100 USD',
+//         imageUrl: 'lib/images/hoodie2.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '75 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '50 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '50 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     Product(
+//         name: 'Pink Hoodie',
+//         price: '100 USD',
+//         imageUrl: 'lib/images/hoodie2.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '75 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     Product(
+//         name: 'Light Purple Hoodie',
+//         price: '50 USD',
+//         imageUrl: 'lib/images/hoodie1.png'),
+//     // Thêm nhiều sản phẩm khác vào danh sách products
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.builder(
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 2,
+//         crossAxisSpacing: 8,
+//         mainAxisSpacing: 8,
+//         childAspectRatio: 0.8,
+//       ),
+//       itemCount: products.length,
+//       itemBuilder: (context, index) {
+//         final product = products[index];
+
+//         return GestureDetector(
+//             onTap: () {
+//               // Xử lý khi bạn click vào item
+//               print('Bạn đã click vào sản phẩm: ${product.name}');
+//               Navigator.pushNamed(context, '/detaiproduct');
+//             },
+//             child: Card(
+//               color: Colors.white,
+//               shadowColor: Colors.black,
+//               elevation: 10,
+//               child: SizedBox(
+//                 child: Stack(
+//                   children: [
+//                     Column(
+//                       children: [
+//                         Expanded(
+//                           flex: 4,
+//                           child: Stack(
+//                             children: [
+//                               Container(
+//                                 width: double.infinity,
+//                                 padding: const EdgeInsets.only(top: 10),
+//                                 child: Image.asset(
+//                                   product.imageUrl,
+//                                   height: 200,
+//                                   width: 180,
+//                                 ),
+//                               ),
+//                               Positioned(
+//                                 top: 5,
+//                                 right: 5,
+//                                 child: IconButton(
+//                                   icon: Icon(
+//                                     product.isFavorite
+//                                         ? Icons.favorite
+//                                         : Icons.favorite_border,
+//                                     color: Colors.white,
+//                                   ),
+//                                   onPressed: () {
+//                                     setState(() {
+//                                       product.isFavorite = !product.isFavorite;
+//                                     });
+//                                   },
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                         Expanded(
+//                           flex: 1,
+//                           child: Container(
+//                             child: Text(
+//                               product.name,
+//                               style: const TextStyle(
+//                                 fontWeight: FontWeight.bold,
+//                                 color: Colors.black,
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                         Expanded(
+//                           flex: 1,
+//                           child: Container(
+//                             padding: EdgeInsets.only(bottom: 10),
+//                             child: Text(
+//                               product.price,
+//                               style: const TextStyle(
+//                                 fontWeight: FontWeight.bold,
+//                                 color: Color.fromARGB(255, 105, 105, 105),
+//                               ),
+//                             ),
+//                           ),
+//                         )
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ));
+//       },
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+import 'package:appclient/Screen/DetailProduct.dart';
+import 'package:appclient/models/productModel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ProductList extends StatefulWidget {
   const ProductList({super.key});
@@ -10,49 +165,27 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
-  List<Product> products = [
-    Product(
-        name: 'Pink Hoodie',
-        price: '100 USD',
-        imageUrl: 'lib/images/hoodie2.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '75 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '50 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    Product(
-        name: 'Pink Hoodie',
-        price: '100 USD',
-        imageUrl: 'lib/images/hoodie2.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '75 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '50 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '50 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    Product(
-        name: 'Pink Hoodie',
-        price: '100 USD',
-        imageUrl: 'lib/images/hoodie2.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '75 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    Product(
-        name: 'Light Purple Hoodie',
-        price: '50 USD',
-        imageUrl: 'lib/images/hoodie1.png'),
-    // Thêm nhiều sản phẩm khác vào danh sách products
-  ];
+  List<productModel> products = []; // Danh sách sản phẩm từ API
+
+  // Hàm để gọi API và cập nhật danh sách sản phẩm
+  Future<void> fetchProducts() async {
+    final response = await http.get(Uri.parse(
+        'http://192.168.45.108:6868/api/products')); // Thay thế URL của API sản phẩm
+    if (response.statusCode == 200) {
+      final List<dynamic> productData = jsonDecode(response.body);
+      setState(() {
+        products =
+            productData.map((item) => productModel.fromJson(item)).toList();
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Gọi API khi widget được khởi tạo
+    fetchProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,84 +201,90 @@ class _ProductListState extends State<ProductList> {
         final product = products[index];
 
         return GestureDetector(
-            onTap: () {
-              // Xử lý khi bạn click vào item
-              print('Bạn đã click vào sản phẩm: ${product.name}');
-              Navigator.pushNamed(context, '/detaiproduct');
-            },
-            child: Card(
-              color: Colors.white,
-              shadowColor: Colors.black,
-              elevation: 10,
-              child: SizedBox(
-                child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        Expanded(
-                          flex: 4,
-                          child: Stack(
-                            children: [
-                              Container(
-                                width: double.infinity,
-                                padding: const EdgeInsets.only(top: 10),
-                                child: Image.asset(
-                                  product.imageUrl,
-                                  height: 200,
-                                  width: 180,
-                                ),
-                              ),
-                              Positioned(
-                                top: 5,
-                                right: 5,
-                                child: IconButton(
-                                  icon: Icon(
-                                    product.isFavorite
-                                        ? Icons.favorite
-                                        : Icons.favorite_border,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      product.isFavorite = !product.isFavorite;
-                                    });
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            child: Text(
-                              product.name,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              product.price,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Color.fromARGB(255, 105, 105, 105),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
+          onTap: () {
+            // Xử lý khi bạn click vào item
+            print('Bạn đã click vào sản phẩm: ${product.name}');
+            // Navigator.pushNamed(context, '/detaiproduct');
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailProduct(
+                  title: 'Chi tiết sản phẩm',
+                  product: product, // Truyền đối tượng sản phẩm đã được chọn
                 ),
               ),
-            ));
+            );
+          },
+          child: Card(
+            color: Colors.white,
+            shadowColor: Colors.black,
+            elevation: 10,
+            child: SizedBox(
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.only(top: 10),
+                              child: Image.memory(
+                                base64Decode(product.image?.elementAt(0) ??
+                                    ''), // Giả sử danh sách ảnh là danh sách base64
+                                height: 200,
+                                width: 180,
+                              ),
+                            ),
+                            Positioned(
+                              top: 5,
+                              right: 5,
+                              child: IconButton(
+                                icon: Icon(Icons.favorite_border_outlined),
+                                onPressed: () {
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                          child: Text(
+                            product.name ?? 'Unknown',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            '\$${product.price ?? 'Unknown Price'}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 105, 105, 105),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
       },
     );
   }
---
+++ lib/models/productModel.dart
@@ -0,0 +1,44 @@
+class productModel {
+  String? sId;
+  String? name;
+  String? description;
+  List<String>? image;
+  String? createdAt;
+  String? updatedAt;
+  String? price;
+  int? iV;
+
+  productModel(
+      {this.sId,
+      this.name,
+      this.description,
+      this.image,
+      this.createdAt,
+      this.updatedAt,
+      this.price,
+      this.iV});
+
+  productModel.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    name = json['name'];
+    description = json['description'];
+    image = json['image'].cast<String>();
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    price = json['price'];
+    iV = json['__v'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    data['image'] = this.image;
+    data['createdAt'] = this.createdAt;
+    data['updatedAt'] = this.updatedAt;
+    data['price'] = this.price;
+    data['__v'] = this.iV;
+    return data;
+  }
+}
