90e010e79b4395a54a3b4ae3be8351fac1bb1437
tran van trong <trongtvph21826@fpt.edu.vn>
2023-11-16 06:16:18 +0700

    call api favorite

--- lib/Screen/Favorite.dart
+++ lib/Screen/Favorite.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:core';
 
+import 'package:appclient/models/productFvoriteModel.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -14,27 +15,40 @@ class Favorite extends StatefulWidget {
   State<Favorite> createState() => _FavoriteState();
 }
 
-
 class _FavoriteState extends State<Favorite> {
-  List<productModel> products = []; // Danh sách sản phẩm từ API
+  List<ListFavorite> products = []; // Danh sách sản phẩm từ API
 
-    Future<void> fetchProducts() async {
+  Future<void> fetchProducts() async {
     final response = await http.get(Uri.parse(
-        'http://localhost:6868/api/getListFavorite/')); // Thay thế URL của API sản phẩm
+        'http://192.168.45.105:6868/api/getListFavorite/6549d3feffe41106e077bd42')); // Thay thế URL của API sản phẩm
+
     if (response.statusCode == 200) {
-      final List<dynamic>? productData = jsonDecode(response.body);
-      if (productData != null && mounted) {
-        setState(() {
-          products = products +
-              productData.map((item) => productModel.fromJson(item)).toList();
-        });
+      try {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+
+        if (responseData.containsKey('listFavorite') &&
+            responseData['listFavorite'] is Map) {
+          final Map<String, dynamic> favoriteData =
+              responseData['listFavorite'] as Map<String, dynamic>;
+
+          setState(() {
+            products = [ListFavorite.fromJson(favoriteData)];
+          });
+        } else {
+          print('Invalid data format: listFavorite is not a Map');
+        }
+      } catch (e) {
+        print('Error parsing JSON: $e');
       }
+    } else {
+      print('Request failed with status: ${response.statusCode}');
     }
   }
 
-    @override
+  @override
   void initState() {
     super.initState();
+    fetchProducts();
   }
 
   @override
@@ -64,7 +78,117 @@ class _FavoriteState extends State<Favorite> {
               color: Colors.white,
               width: double.infinity,
               height: 620,
-              child: const Text("data"),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  crossAxisSpacing: 6,
+                  mainAxisSpacing: 6,
+                  childAspectRatio: 2.2,
+                ),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  if (index < products.length) {
+                    final product = products[index];
+                    print(product.productId?.name);
+                    return GestureDetector(
+                      child: Card(
+                        child: Stack(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(10),
+                                    child: Image.memory(
+                                      base64Decode(product.productId?.image
+                                              ?.elementAt(0) ??
+                                          ''),
+                                      height: 200,
+                                      width: 180,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 6,
+                                  child: Container(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        0, 30, 10, 20),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Expanded(
+                                          flex: 7,
+                                          child: Text(
+                                            product.productId?.name ??
+                                                "Unknown",
+                                            style: const TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xff6342E8)),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex: 4,
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                '\$${product.productId?.price ?? 'Unknown Price'}',
+                                                style: const TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              Container(
+                                                decoration: BoxDecoration(
+                                                    color: Color(0xff6342E8),
+                                                    borderRadius:
+                                                        BorderRadius.all(
+                                                            Radius.circular(
+                                                                10))),
+                                                padding: EdgeInsets.all(5),
+                                                child: Text("Add to cart",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: IconButton(
+                                icon: const Icon(Icons.close,
+                                    color: Colors.black),
+                                onPressed: () {
+                                  // Xử lý khi nút "x" được nhấn
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    if (mounted) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }
+                  return null;
+                },
+              ),
             ),
           ),
           Align(
