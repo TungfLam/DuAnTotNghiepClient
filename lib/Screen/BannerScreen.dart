9402ee965887484779aa8b2205cbf5d3f4a0e6b9
tran van trong <trongtvph21826@fpt.edu.vn>
2023-12-24 08:07:18 +0700

    edit_login_register

--- lib/Screen/BannerScreen.dart
+++ lib/Screen/BannerScreen.dart
@@ -1,4 +1,7 @@
+
+import 'package:concentric_transition/concentric_transition.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BannerScreen extends StatefulWidget {
   const BannerScreen({Key? key, required this.title}) : super(key: key);
@@ -8,7 +11,52 @@ class BannerScreen extends StatefulWidget {
   State<BannerScreen> createState() => _BannerScreenState();
 }
 
-class _BannerScreenState extends State<BannerScreen> {
+class _BannerScreenState extends State<BannerScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+
+    Future<void> checkLogin() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool? isLogin = prefs.getBool("isLogin");
+    final String? idUser = prefs.getString("idUser");
+    if (isLogin != null) {
+      if (isLogin == true) {
+        print("người dùng đã login");
+        Navigator.pushNamed(context, '/');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkLogin();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset(0.0, 0.4),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -18,41 +66,67 @@ class _BannerScreenState extends State<BannerScreen> {
           width: double.infinity,
           height: double.infinity,
           decoration: const BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage('lib/images/banner.jpg'),
-            fit: BoxFit.cover,
-          )),
-          child: Container(
-            child: Padding(
-            padding: EdgeInsets.only(bottom: 47),
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(padding: EdgeInsets.only(bottom: 40),
-                    child: Text(
-                      "Adadas",
-                       style: TextStyle(fontSize: 80,fontWeight: FontWeight.w900,color: Colors.white),
-                   ),
+            color: Color(0xff9975ff),
+          ),
+          child: SlideTransition(
+            position: _offsetAnimation,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('lib/images/banner.png'),
+                fit: BoxFit.cover,
+              )),
+              child: SlideTransition(
+                position: _offsetAnimation,
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 50),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 30),
+                          child: Text(
+                            "Adadas",
+                            style: TextStyle(
+                              fontSize: 80,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 200,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.white),
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/');
+                            },
+                            child: Text(
+                              'Xem ngay',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                TextButton(
-                child: Text("SHOP NOW".toUpperCase(),
-                style: TextStyle(fontSize: 21)),
-                style: ButtonStyle(
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                        EdgeInsets.only(left: 80,right: 80,top: 30,bottom: 30)),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40),
-                            side: BorderSide( width:3.0,color: Colors.white)))),
-                onPressed: () => Navigator.pushNamed(context, '/loginorregister')),
-                ],
-              )
-            ) 
+                ),
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
